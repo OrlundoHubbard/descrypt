c9279618c5c7362a13c54d65df71ab8b276d5a6a
Orlundo Hubbard <orlundo@wursta.com>
2022-09-12 20:55:44 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+   <h1>Write Copy That Grabs <br>Your</br> Customers Attention</h1>
+    </>
+  )
+}
